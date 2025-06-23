64d02b0eadcad88a0a2b4e21b78887d19a6bea20
itaebro <plus100kt@gmail.com>
2025-06-23 17:34:19 +0900

    init: 이커머스 프로젝트 생성

---
+++ src/test/kotlin/com/echo/ecommerce/EcommerceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.echo.ecommerce
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
